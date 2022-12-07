249fc5abf8dd42073e4fc6e273190ed062e8fe72
TeamC4-GP <TeamC4@GaryPedretti.com>
2022-12-07 16:10:39 +0000

    Print all names

--- Battleship.Ascii/Program.cs
+++ Battleship.Ascii/Program.cs
@@ -258,12 +258,20 @@ namespace Battleship.Ascii
         {
             if (GameController.AllEnemyShipsSunk(enemyFleet))
             {
+                foreach (var ship in enemyFleet)
+                {
+                    Console.WriteLine("Enemy ship {0} is destroyed!", ship.Name);
+                }
                 Console.WriteLine("You are the winner!");
                 return true;
             };
 
             if (GameController.AllMyShipsSunk(myFleet))
             {
+                foreach (var ship in myFleet)
+                {
+                    Console.WriteLine("Your ship {0} is destroyed!", ship.Name);
+                }
                 Console.WriteLine("You");
                 return true;
             }
